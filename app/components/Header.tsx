bc202b3a5acbb61ceb9ea61f53e3b66599037de3
Pavlo Surzhenko <pavel.surzhenko@icloud.com>
2023-07-28 15:01:50 +0300

    add responsive to header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,8 +2,9 @@
 import Link from 'next/link';
 import { styled } from 'styled-components';
 import Container from './Container';
-import { useContext } from 'react';
+import { HTMLAttributes, useContext, useState, useEffect } from 'react';
 import { CartContext } from './CartContext';
+import NavIcon from './icons/NavIcon';
 
 const StyledHeader = styled.header`
     background-color: #222;
@@ -12,6 +13,8 @@ const StyledHeader = styled.header`
 const Logo = styled(Link)`
     color: #fff;
     text-decoration: none;
+    position: relative;
+    z-index: 3;
 `;
 
 const Wrapper = styled.div`
@@ -21,24 +24,65 @@ const Wrapper = styled.div`
 `;
 
 const NavLink = styled(Link)`
+    display: block;
     color: #aaa;
     text-decoration: none;
+    padding: 10px 0px;
+    @media screen and (min-width: 768px) {
+        padding: 0;
+    }
 `;
 
-const StyledNav = styled.nav`
-    display: flex;
+const StyledNav = styled.nav<StyledNavProps>`
+    display: ${(props) => (props.$mobileNavActive ? 'block' : 'none')};
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    padding: 70px 20px 20px;
+    background-color: #222;
     gap: 15px;
+
+    @media screen and (min-width: 768px) {
+        display: flex;
+        position: static;
+        padding: 0;
+    }
+`;
+
+const NavButton = styled.button`
+    background-color: transparent;
+    border: 0;
+    width: 30px;
+    height: 30px;
+    color: #fff;
+    cursor: pointer;
+    position: relative;
+    z-index: 3;
+    @media screen and (min-width: 768px) {
+        display: none;
+    }
 `;
 
 const Header = () => {
     const { cartProducts } = useContext(CartContext);
+    const [navActive, setNavActive] = useState(false);
+
+    useEffect(() => {
+        if (navActive) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+    }, [navActive]);
 
     return (
         <StyledHeader>
             <Container>
                 <Wrapper>
                     <Logo href={'/'}>Ecommerce</Logo>
-                    <StyledNav>
+                    <StyledNav $mobileNavActive={navActive}>
                         <NavLink href={'/'}>Home</NavLink>
                         <NavLink href={'/products'}>All products</NavLink>
                         <NavLink href={'/categories'}>Categories</NavLink>
@@ -47,9 +91,16 @@ const Header = () => {
                             Cart ({cartProducts.length})
                         </NavLink>
                     </StyledNav>
+                    <NavButton onClick={() => setNavActive((prev) => !prev)}>
+                        <NavIcon />
+                    </NavButton>
                 </Wrapper>
             </Container>
         </StyledHeader>
     );
 };
 export default Header;
+
+interface StyledNavProps extends HTMLAttributes<HTMLElement> {
+    $mobileNavActive: boolean;
+}
