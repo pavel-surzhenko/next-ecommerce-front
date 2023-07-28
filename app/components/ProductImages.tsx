cea397e976dc7d648d8ae2ed1408e868e17e3561
Pavlo Surzhenko <pavel.surzhenko@icloud.com>
2023-07-28 11:31:14 +0300

    add img list

--- app/components/ProductImages.tsx
+++ app/components/ProductImages.tsx
@@ -1,28 +1,29 @@
 import Image from 'next/image';
 import { IProduct } from '../page';
 import { styled } from 'styled-components';
+import { useState } from 'react';
 
 const ImageButtons = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
+    gap: 5px;
+    margin-top: 10px;
+`;
+
+const ImageButton = styled.div`
+    border: 2px solid #ccc;
+    padding: 5px;
+    cursor: pointer;
+    border-radius: 5px;
 `;
 
 export const ProductImages = (productInfo: IProduct) => {
-    const ImageButtons = styled.div`
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-        gap: 5px;
-    `;
-
-    const ImageButton = styled.div`
-        border: 1px solid #aaa;
-        padding: 5px;
-    `;
+    const [activeImg, setActiveImg] = useState(productInfo.images[0]);
 
     return (
         <>
             <Image
-                src={productInfo?.images[0]!}
+                src={activeImg}
                 width={1000}
                 height={1000}
                 style={{
@@ -34,13 +35,16 @@ export const ProductImages = (productInfo: IProduct) => {
             />
             <ImageButtons>
                 {productInfo.images.map((image) => (
-                    <ImageButton key={image}>
+                    <ImageButton
+                        key={image}
+                        onClick={() => setActiveImg(image)}
+                    >
                         <Image
                             src={image}
                             width={1000}
                             height={1000}
                             style={{
-                                width: '100px',
+                                width: '70px',
                                 height: 'auto',
                                 objectFit: 'contain',
                             }}
